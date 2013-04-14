fba5af555e5176a9743672f1cc587240b2496468
Nic Ferier <nic@ferrier.me.uk>
2013-04-14 17:02:24 +0100

    bump the version

--- elpakit.el
+++ elpakit.el
@@ -7,7 +7,7 @@
 ;; URL: http://github.com/nicferrier/elpakit
 ;; Keywords: lisp
 ;; Package-Requires: ((anaphora "0.0.6")(dash "1.0.3"))
-;; Version: 1.0.0
+;; Version: 1.0.1
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
