93158d4d21ac9b8de6ff7cf7d07f1d7ff2d3f7d0
Nic Ferrier <nferrier@ferrier.me.uk>
2014-11-10 18:37:16 +0000

    add files for fake-package test

---
+++ fake-package-with-elisp/fake-package.el
@@ -0,0 +1,31 @@
+;;; fake-package.el --- just a fake package for elpakit testing  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2014  Nic Ferrier
+
+;; Author: Nic Ferrier <nferrier@ferrier.me.uk>
+;; Keywords: lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This is just a fake package, not useful at all.
+
+
+;;; Code:
+
+
+
+(provide 'fake-package)
+;;; fake-package.el ends here
---
+++ fake-package-with-elisp/test-stuff/test-file.el
@@ -0,0 +1,6 @@
+;;; just a simple test file
+
+;; this is just a test for elpakit
+
+
+;;; test-file.el ends here
